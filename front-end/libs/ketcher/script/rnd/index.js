d0b679688deb5ed6512a9b5e19afc02ff6f4fa2d
jacr1 <jacrivett9@gmail.com>
2018-04-16 23:12:30 +0100

    removed files from lib and created a npm install file

--- front-end/libs/ketcher/script/rnd/index.js
+++
@@ -1,9 +0,0 @@
-/*global require, global:false*/
-
-require('./visel');
-require('./restruct');
-require('./editor');
-require('./render');
-require('./restruct_rendering');
-
-global.rnd = global.rnd || {};
